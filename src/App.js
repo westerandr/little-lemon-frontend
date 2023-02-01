8d2652642d8977347ca658b8853bb1a8c9a408b0
Andre Westerlund <andrewest_04@hotmail.com>
2023-02-01 21:09:33 +1300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Header from './components/Header';
-import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div id="main-content">
       <Header />
-      <Nav />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from '../assets/logo.jpg';
+import Nav from './Nav';
 
 export default function Header(){
   return <header>
     <img src={logo} alt="Little Lemon Logo" />
+    <Nav />
   </header>
 }
